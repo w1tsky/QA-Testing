13a9bfa278a16db3f00799e7b8e6d1e3ee5e3d77
Daniil Marchuk <neill.white220@gmail.com>
2020-02-12 12:54:09 +0300

    Renamed Folders

---
+++ SeleniumWebDriver/UTest/expediaTest.cs
@@ -0,0 +1,89 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumWebDriver
+{
+    [TestFixture]
+    class ExpediaTests
+    {
+        private IWebDriver webDriver;
+
+        [SetUp]
+        public void StartBrowserChrome()
+        {
+            webDriver = new ChromeDriver
+            {
+                Url = "https://www.expedia.com/"
+            };
+        }
+
+        [Test]
+        public void CheckAge()
+        {
+            DateTime dateAndTime = DateTime.Now;
+            var today = dateAndTime.ToString("MM/dd/yyyy");
+
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='package-origin-hp-package']"));
+            var origin = webDriver.FindElement(By.XPath("//*[@id='package-origin-hp-package']"));
+            origin.SendKeys("Sydney");
+
+
+
+
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='package-destination-hp-package']"));
+            var destination = webDriver.FindElement(By.XPath("//*[@id='package-destination-hp-package']"));
+            destination.SendKeys("Gold Coast");
+
+
+
+
+
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='package-departing-hp-package']"));
+            var departing = webDriver.FindElement(By.XPath("//*[@id='package-departing-hp-package']"));
+            departing.SendKeys(today);
+
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='package-returning-hp-package']"));
+            var returning = webDriver.FindElement(By.XPath("//*[@id='package-returning-hp-package']"));
+            returning.SendKeys(today);
+
+            //*[@id='traveler-selector-hp-package']
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='traveler-selector-hp-package']/div/ul/li/button"));
+            var travelers = webDriver.FindElement(By.XPath("//*[@id='traveler-selector-hp-package']/div/ul/li/button"));
+            travelers.Click();
+
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='traveler-selector-hp-package']/div/ul/li/div/div/div[1]/div[3]/div[1]/div[4]/button"));
+            var childrenAdd = webDriver.FindElement(By.XPath("//*[@id='traveler-selector-hp-package']/div/ul/li/div/div/div[1]/div[3]/div[1]/div[4]/button"));
+            childrenAdd.Click();
+            //*[@id='traveler-selector-hp-package']/div/ul/li/button
+
+
+
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='search-button-hp-package']"));
+            var sumbitBtn = webDriver.FindElement(By.XPath("//*[@id='search-button-hp-package']"));
+            sumbitBtn.Click();
+
+            WaitForElementToAppear(webDriver, 15, By.XPath("//*[@id='gcw-packages-form-hp-package']/div[2]/div/ul/li/a"));
+            var errorMessage = webDriver.FindElement(By.XPath("//*[@id='gcw-packages-form-hp-package']/div[2]/div/ul/li/a"));
+            var isErrorMessageCorrect = errorMessage.Text.Equals("Please provide the ages of children below.");
+            Assert.IsTrue(isErrorMessageCorrect);
+        }
+        
+
+     
+
+        public static IWebElement WaitForElementToAppear(IWebDriver driver, int waitTime, By waitingElement)
+        {
+            IWebElement wait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitTime)).Until(ExpectedConditions.ElementExists(waitingElement));
+            return wait;
+        }
+
+        [TearDown]
+        public void QuitDriver()
+        {
+            webDriver.Quit();
+        }
+    }
+}
---
+++ xUnit/TriangleTest/TriangleExist.Test/TriangleTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleLib;
+
+
+namespace TriangleLib
+{
+    [TestClass]
+    public class TriangleExist
+    {
+        [TestMethod]
+        public void IsRightTriangle()
+        {
+            Assert.IsTrue(TriangleTest.IsTriangle(5, 8, 10));
+        }
+
+        [TestMethod]
+        public void AllSidesAreZero()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(0, 0, 0));
+        }
+
+        [TestMethod]
+        public void AllSidesAreNegative()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-5, -8, -10));
+        }
+
+        [TestMethod]
+        public void IsTrianglePossible()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(19, 10, 5));
+        }
+
+        [TestMethod]
+        public void OneSideIsZero()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(0, 3, 7));
+        }
+
+        [TestMethod]
+        public void OneSideIsNegative()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(3, -4, 5));
+        }
+
+        [TestMethod]
+        public void IsTriangleIsoscelesTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(5, 5, 7));
+        }
+
+        [TestMethod]
+        public void IsTriangleEquilateralTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(5, 5, 5));
+        }
+
+        [TestMethod]
+        public void IsAnotherRightTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(4, 7, 10));
+        }
+
+        [TestMethod]
+        public void IsAndAnotherRightTriangle()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(2, 4, 6));
+        }
+    }
+}
